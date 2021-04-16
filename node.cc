3fa963a80e26f98f5976c5286c457cd0eff48647
Lei Yang <leiy@csail.mit.edu>
2021-04-16 01:39:31 +0000

    add mining

--- node.cc
+++ node.cc
@@ -6,31 +6,51 @@ using namespace omnetpp;
 // FullNode is a full node in a blockchain network.
 class FullNode : public cSimpleModule
 {
-  protected:
-    virtual void initialize() override;
-    virtual void handleMessage(cMessage *msg) override;
+	private:
+		cMessage *event;  // pointer to the event object for mining the next block
+
+	public:
+		FullNode();
+		virtual ~FullNode();
+
+	protected:
+		virtual void initialize() override;
+		virtual void handleMessage(cMessage *msg) override;
 };
 
 // Register the module with omnet
 Define_Module(FullNode);
 
+FullNode::FullNode()
+{
+	// ensure the point is null so that destructor will not try to free some random addr
+	event = nullptr;
+}
+
+FullNode::~FullNode()
+{
+	cancelAndDelete(event);
+}
+
 void FullNode::initialize()
 {
-    // Am I Tic or Toc?
-    EV << "sending\n";
-    if (strcmp("tic", getName()) == 0) {
-        // create and send first message on gate "out". "tictocMsg" is an
-        // arbitrary string which will be the name of the message object.
-        cMessage *msg = new cMessage("tictocMsg");
-        send(msg, "out");
-    }
+	// schedule the next block to be mined
+	event = new cMessage("mined");
+	simtime_t timeToNextBlock = exponential(40);	// PoW on node mines one block every 40 seconds
+	scheduleAt(timeToNextBlock, event);
 }
 
 void FullNode::handleMessage(cMessage *msg)
 {
-    // The handleMessage() method is called whenever a message arrives
-    // at the module. Here, we just send it to the other module, through
-    // gate `out'. Because both `tic' and `toc' does the same, the message
-    // will bounce between the two.
-    send(msg, "out"); // send out the message
+	if (msg == event) {
+		EV << getName() << " mined a block\n";
+		cMessage* newBlock = new cMessage("block");
+		send(newBlock, "out");
+		simtime_t timeToNextBlock = exponential(40);	// PoW on node mines one block every 40 seconds
+		scheduleAt(simTime() + timeToNextBlock, event);
+	} else {
+		EV << getName() << " received a block\n";
+		delete msg;	// delete the block
+	}
+
 }
