3d4ff4b14d54d56917d772d89964cdd36f13c003
Lei Yang <leiy@csail.mit.edu>
2021-04-30 14:28:02 +0000

    fix miner

--- LongestChain.cc
+++ LongestChain.cc
@@ -7,22 +7,22 @@ using namespace std;
 
 // LongestChain implements a blockchain miner.
 // It supports three mining modes, each mimics a type of permissionless consensus protocol:
-//   Continuous mode, where blocks are mined as in PoW. Set ronudIntv and numFixedLongestChains
+//   Continuous mode, where blocks are mined as in PoW. Set ronudIntv and numFixedMiners
 //   to zero, and set miningRate to the desired per-node mining rate.
 //
 //   Round-by-round mode, where blocks are mined as in PoS. Set roundIntv to the rount
 //   interval, and set miningRate to the desired per-node mining rate (blocks per
-//   second). Set numFixedLongestChains to zero.
+//   second). Set numFixedMiners to zero.
 //
 //   Fixed miner mode, where the same set of miners mine one block in each round. Set
-//   numFixedLongestChains to the number of miners in each round, and set roundIntv to the
+//   numFixedMiners to the number of miners in each round, and set roundIntv to the
 //   desired round interval. Ignore the mining rate setting.
 class LongestChain: public cSimpleModule
 {
 	private:
 		// parameters
 		unsigned short id;          // id of the node
-		int numFixedLongestChains; // number of miners in each round; used for network experiments
+		int numFixedMiners; // number of miners in each round; used for network experiments
 		double roundTime; // round time (round mode), 0 for continuous time mode
 		cExponential rvBlockDelay; // block inter-arrival time (continuous time mode)
 		cPoisson rvBlocksPerRound; // blocks per round (round mode)
@@ -79,7 +79,7 @@ void LongestChain::initialize()
 	WATCH(nextBlockSeq);
 
 	// set up mining RVs
-	numFixedLongestChains = par("numFixedLongestChains").intValue();
+	numFixedMiners = par("numFixedMiners").intValue();
 	roundTime = par("roundIntv").doubleValueInUnit("s");
 	double miningRate = par("miningRate").doubleValue(); // in blocks per second
 	if (roundTime == 0.0) {
@@ -140,8 +140,8 @@ void LongestChain::handleMessage(cMessage *msg)
 		}
 		else {
 			int nBlocks;
-			if (numFixedLongestChains > 0) {
-				nBlocks = id < numFixedLongestChains? 1 : 0;
+			if (numFixedMiners > 0) {
+				nBlocks = id < numFixedMiners? 1 : 0;
 			}
 			else {
 				nBlocks = int(rvBlocksPerRound.draw());
